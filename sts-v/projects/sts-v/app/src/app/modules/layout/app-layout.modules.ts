04789a0f4c75c09fa1970a3ea8242530304a78e0
Adrian Bratu <adrian.bratu@amrock.com>
2022-02-22 10:00:11 -0500

    add colors

--- sts-v/projects/sts-v/app/src/app/modules/layout/app-layout.modules.ts
+++ sts-v/projects/sts-v/app/src/app/modules/layout/app-layout.modules.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 import { RouterModule } from '@angular/router';
 import { AppLayoutContainerComponent } from './components/layout-container/app-layout-container.component';
 import { AppLayoutFooterComponent } from './components/layout-footer/app-layout-footer.component';
@@ -14,9 +16,15 @@ import { AppLayoutComponent } from './components/layout/app-layout.component';
     AppLayoutHeaderComponent,
     AppLayoutFooterComponent,
     AppLayoutNavComponent,
-    AppLayoutContainerComponent
+    AppLayoutContainerComponent,
   ],
-  imports: [CommonModule, FlexLayoutModule, RouterModule],
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatDividerModule,
+    RouterModule,
+  ],
   exports: [AppLayoutComponent],
 })
 export class AppLayoutModule {}
